be3a44eaabe5a2bdfc1cfd72091070155d024e54
alankolk <73644951+alankolk@users.noreply.github.com>
2021-04-04 14:14:34 +0300

    Create Kaitserüü.java

---
+++ src/Kaitserüü.java
@@ -0,0 +1,11 @@
+public class Kaitserüü extends Varustus{
+    int eludelisa;
+
+    public Kaitserüü(int eludelisa) {
+        this.eludelisa = eludelisa;
+    }
+
+    public int getEludelisa() {
+        return eludelisa;
+    }
+}
