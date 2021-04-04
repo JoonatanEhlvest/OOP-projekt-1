fdc54b200fac6472d283f09237af9ca28a8131a1
alankolk <73644951+alankolk@users.noreply.github.com>
2021-04-04 13:44:40 +0300

    Update Koobas.java

--- src/Koobas.java
+++ src/Koobas.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Koobas {
+    private List<Koobas> ruumid = new ArrayList<>();
+    private Random suvalineArv = new Random();
+
+    public Koobas suvalineRuum(){
+        return ruumid.get(suvalineArv.nextInt(ruumid.size()));
+    }
+
+    public void lisaRuum(Koobas ruum) {
+        ruumid.add(ruum);
+    }
+
 }
