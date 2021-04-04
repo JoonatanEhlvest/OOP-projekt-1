11ee51a90b889b827bf526f1fb27af15ea833f1a
alankolk <73644951+alankolk@users.noreply.github.com>
2021-04-04 14:14:43 +0300

    Update Relv.java

--- src/Relv.java
+++ src/Relv.java
@@ -1,6 +1,11 @@
 public class Relv extends Varustus{
     int rünnak;
-    int nimi;
+    String nimi;
+
+    public Relv(int rünnak, String nimi) {
+        this.rünnak = rünnak;
+        this.nimi = nimi;
+    }
 
     public int getRünnak() {
         return rünnak;
