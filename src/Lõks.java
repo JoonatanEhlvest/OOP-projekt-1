5df850e6a286143b04108c30d1c2ba9dc2d2b0e3
JoonatanEhlvest <joonatanehlvest123@gmail.com>
2021-04-07 17:49:40 +0300

    Muutsin aktiveerumislävendi aktiveerumistõenäosuseks

--- src/Lõks.java
+++ src/Lõks.java
@@ -1,17 +1,17 @@
 public class Lõks {
     private int kahju;
-    private double aktiveerumisLävend;
+    private double aktiveerumisTõenäosus;
     private boolean ignoreeribKaitset;
 
-    public Lõks(int kahju, double aktiveerumisLävend, boolean ignoreeribKaitset) {
+    public Lõks(int kahju, double aktiveerumisTõenäosus, boolean ignoreeribKaitset) {
         this.kahju = kahju;
-        this.aktiveerumisLävend = aktiveerumisLävend;
+        this.aktiveerumisTõenäosus = aktiveerumisTõenäosus;
         this.ignoreeribKaitset = ignoreeribKaitset;
     }
 
     public void aktiveerumine(Olend ohver) {
         int õnn = Juhuslik.randint(1, 100);
-        if (õnn <= aktiveerumisLävend) {
+        if (õnn <= aktiveerumisTõenäosus) {
             System.out.println("Lõks aktiveerus");
             ohver.võtabKahju(kahju, ignoreeribKaitset);
         }
