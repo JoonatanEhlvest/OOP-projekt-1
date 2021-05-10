da5729ceb8ab38c3a20b08c04122c0fa758535f6
alankolk <73644951+alankolk@users.noreply.github.com>
2021-05-10 22:09:35 +0300

    Update Peaklassfx.java

--- src/main/java/oop/Peaklassfx.java
+++ src/main/java/oop/Peaklassfx.java
@@ -1,6 +1,8 @@
 package oop;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -29,14 +31,39 @@ public class Peaklassfx extends Application {
         imageView.setFitWidth(1280);
         imageView.setFitHeight(720);
 
+
         BorderPane bp = new BorderPane();
         bp.getChildren().add(imageView);
 
         root.getChildren().add(bp);
         Scene scene = new Scene(root, 1280, 720);
 
-        pealava.minWidthProperty().bind(scene.heightProperty().multiply(1.77777777));
-        pealava.minHeightProperty().bind(scene.widthProperty().divide(1.77777777));
+        double suhe = 16/9;
+        pealava.minWidthProperty().bind(scene.heightProperty().multiply(1.777));
+        pealava.minHeightProperty().bind(scene.widthProperty().divide(1.777));
+
+
+        /**
+         * NEED KAKS KUULARIT MUUDAVAD NENDE ASJADE SUURUST, MIS SIIN KIRJELDATUD ON, hetkel ainult tausta
+         * Aga siia lisada veel peategelane, vastane ja siis nupud jne.
+         * Ja pärast tsüklis saame omistada uusi asju nendele imageView-dele ja vastase isendile
+         */
+        pealava.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                imageView.setFitWidth((double) newValue);
+            }
+        });
+
+        pealava.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                imageView.setFitHeight((double) newValue);
+
+            }
+        });
+
+
         pealava.setScene(scene);
         pealava.show();
     }
