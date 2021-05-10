09863b0dd66a43d5adc1ba59f975673d95456826
JoonatanEhlvest <joonatanehlvest123@gmail.com>
2021-05-10 17:14:22 +0300

    Update Peaklassfx.java

--- src/main/java/oop/Peaklassfx.java
+++ src/main/java/oop/Peaklassfx.java
@@ -1,8 +1,12 @@
 package oop;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.stage.Stage;
-
 public class Peaklassfx extends Application {
 
     public static void main(String[] args) {
@@ -12,6 +16,11 @@ public class Peaklassfx extends Application {
     @Override
     public void start(Stage pealava) {
         pealava.setTitle("Basic Dungeon");
+
+        BackgroundImage taustapilt = new BackgroundImage(
+                new Image("images/Taust.jpg"), BackgroundRepeat.ROUND, BackgroundRepeat.ROUND, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+
 
         pealava.show();
     }
