ae498b5ef7cf7bc05a8c54bf4439274026a3312a
alankolk <73644951+alankolk@users.noreply.github.com>
2021-05-10 17:52:16 +0300

    Update Peaklassfx.java

--- src/main/java/oop/Peaklassfx.java
+++ src/main/java/oop/Peaklassfx.java
@@ -8,7 +8,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
