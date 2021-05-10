3c4f50b9568c4a01d88c7d7ef6985d44edf092e0
alankolk <73644951+alankolk@users.noreply.github.com>
2021-05-10 17:29:09 +0300

    Pildid

--- src/main/java/oop/Peaklassfx.java
+++ src/main/java/oop/Peaklassfx.java
@@ -1,12 +1,21 @@
 package oop;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class Peaklassfx extends Application {
 
     public static void main(String[] args) {
@@ -17,11 +26,25 @@ public class Peaklassfx extends Application {
     public void start(Stage pealava) {
         pealava.setTitle("Basic Dungeon");
 
-        BackgroundImage taustapilt = new BackgroundImage(
-                new Image("images/Taust.jpg"), BackgroundRepeat.ROUND, BackgroundRepeat.ROUND, BackgroundPosition.DEFAULT,
-                BackgroundSize.DEFAULT);
+        Image taust = pilt("images/Taust.jpg");
+        ImageView imageView = new ImageView();
+        imageView.setImage(taust);
 
+        Group root = new Group(imageView);
+        Scene scene = new Scene(root, 595, 370);
 
+        pealava.setScene(scene);
         pealava.show();
+    }
+
+    public static Image pilt (String path) {
+        try {
+            InputStream is = new FileInputStream(path);
+            Image image = new Image(is);
+            return image;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
