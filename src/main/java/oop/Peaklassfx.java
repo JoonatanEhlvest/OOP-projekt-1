e8221a130c5efb9c3f22b722dc01e59c2995f6bf
JoonatanEhlvest <joonatanehlvest123@gmail.com>
2021-05-11 10:27:10 +0300

    Tegin prooviks mingid nupud. Vb oleks parem Hboxiga teha

--- src/main/java/oop/Peaklassfx.java
+++ src/main/java/oop/Peaklassfx.java
@@ -1,11 +1,15 @@
 package oop;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
@@ -29,10 +33,35 @@ public class Peaklassfx extends Application {
         taustapilt.setFitHeight(720);
 
 
-        //TODO erinevad stseenid: menüü, liikumine, võitlus
+        //TODO erinevad stseenid: menüü, liikumine, võitlus, managa ründmine? (võib ka nt pop up window olla)
 
         BorderPane bp = new BorderPane();
         bp.getChildren().add(taustapilt);
+
+        // Nupud:
+        GridPane gridValikud = new GridPane();
+        gridValikud.setPadding(new Insets(15, 15, 15, 15));
+        gridValikud.setVgap(10);
+        gridValikud.setHgap(10);
+
+        // Nupp 1
+        Button valik1 = new Button("Liigu edasi");
+        GridPane.setConstraints(valik1, 0, 0);
+        // Nupp 2
+        Button valik2 = new Button("Vaata kaarti");
+        GridPane.setConstraints(valik2, 0, 1);
+        // Nupp 3
+        Button valik3 = new Button("Vaata varustust");
+        GridPane.setConstraints(valik3, 1, 0);
+        // Nupp 4
+        Button valik4 = new Button("Salvesta ja välju");
+        GridPane.setConstraints(valik4, 1, 1);
+
+
+        gridValikud.getChildren().addAll(valik1, valik2, valik3, valik4);
+        gridValikud.setAlignment(Pos.CENTER);
+        bp.setBottom(gridValikud);
+
 
         Scene scene = new Scene(bp, 1280, 720);
 
